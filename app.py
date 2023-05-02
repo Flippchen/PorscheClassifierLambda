c225d8cb9f61cd0c0d7f1f8cd30a0aac7a110e9b
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-05-02 18:31:16 +0200

    hopefully last fix

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from utilities.tools import classify_image_process
 def handler(event, context):
     # Get the data from the request
     # Extract the image data and model_name
-    image_data = event.get('image_data')
-    model_name = event.get('model_name')
-    print(event)
+    body = event.get('body')
+
+    image_data = body.get('image_data')
+    model_name = body.get('model_name')
+    print(body)
     # Convert the base64 image data to bytes
     top3_predictions = classify_image_process(image_data, model_name)
 
