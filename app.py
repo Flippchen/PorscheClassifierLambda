39135c6a4729a959b5963c488888c4a24374414f
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-05-02 16:32:47 +0200

    added lambda handler

---
+++ app.py
@@ -0,0 +1,15 @@
+import sys
+import json
+from utilities.tools import classify_image_process
+
+
+def handler(event, context):
+    # Get the data from the request
+    # Extract the image data and model_name
+    image_data = event.get('image_data')
+    model_name = event.get('model_name')
+
+    # Convert the base64 image data to bytes
+    top3_predictions = classify_image_process(image_data, model_name)
+
+    return top3_predictions
